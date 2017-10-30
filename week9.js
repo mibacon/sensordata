c2a0dc7da89204537aee2283caa7e6c25c46e7c1
michal <hush.some1@gmail.com>
2017-10-30 14:09:18 +0000

    code

--- week9.js
+++ week9.js
@@ -7,37 +7,38 @@ var access_token = process.env.PHOTON_TOKEN;
 var particle_variable = 'json';
 var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 
+console.log(device_url)
 // AWS RDS POSTGRESQL INSTANCE
-var db_credentials = new Object();
-db_credentials.user = 'mibacon';
-db_credentials.host = process.env.AWSRDS_EP;
-db_credentials.database = 'sensor';
-db_credentials.password = process.env.AWSRDS_PW;
-db_credentials.port = 5432;
+// var db_credentials = new Object();
+// db_credentials.user = 'mibacon';
+// db_credentials.host = process.env.AWSRDS_EP;
+// db_credentials.database = 'sensor';
+// db_credentials.password = process.env.AWSRDS_PW;
+// db_credentials.port = 5432;
 
-var getAndWriteData = function () {
-    // Make request to the Particle API to get sensor values
-    request(device_url, function (error, response, body) {
-        // Store sensor values in variables
-        var device_json_string = JSON.parse(body).result;
-        var fsr = JSON.parse(device_json_string).fsr;
-        var temp = JSON.parse(device_json_string).temp;
+// var getAndWriteData = function () {
+//     // Make request to the Particle API to get sensor values
+//     request(device_url, function (error, response, body) {
+//         // Store sensor values in variables
+//         var device_json_string = JSON.parse(body).result;
+//         var fsr = JSON.parse(device_json_string).fsr;
+//         var temp = JSON.parse(device_json_string).temp;
 
-        // Connect to the AWS RDS Postgres database
-        const client = new Client(db_credentials);
-        client.connect();
+//         // Connect to the AWS RDS Postgres database
+//         const client = new Client(db_credentials);
+//         client.connect();
 
-        // Construct a SQL statement to insert sensor values into a table
-        var thisQuery = "INSERT INTO sensordata VALUES (" + fsr + "," + temp + ", DEFAULT);";
-        console.log(thisQuery); // for debugging
+//         // Construct a SQL statement to insert sensor values into a table
+//         var thisQuery = "INSERT INTO sensordata VALUES (" + fsr + "," + temp + ", DEFAULT);";
+//         console.log(thisQuery); // for debugging
 
-        // Connect to the AWS RDS Postgres database and insert a new row of sensor values
-        client.query(thisQuery, (err, res) => {
-            console.log(err, res);
-            client.end();
-        });
-    });
-};
+//         // Connect to the AWS RDS Postgres database and insert a new row of sensor values
+//         client.query(thisQuery, (err, res) => {
+//             console.log(err, res);
+//             client.end();
+//         });
+//     });
+// };
 
-// write a new row of sensor data every five minutes
-setInterval(getAndWriteData, 300000);
+// // write a new row of sensor data every five minutes
+// setInterval(getAndWriteData, 300000);
